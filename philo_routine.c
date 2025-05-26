0cdd363ee13f5e99c3c212efd96e3ad1fcd83e04
Hamza <hbelaih@student.42amman.com>
2025-05-26 15:55:43 +0300

    working

--- philo_routine.c
+++ philo_routine.c
@@ -6,7 +6,7 @@
 /*   By: hbelaih <hbelaih@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 02:11:50 by hamzabillah       #+#    #+#             */
-/*   Updated: 2025/05/14 17:58:40 by hbelaih          ###   ########.fr       */
+/*   Updated: 2025/05/26 15:54:31 by hbelaih          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,26 +59,26 @@ void	check_routine(t_philo *philo, t_data *data)
 
 void	*philo_routine(void *arg)
 {
-	t_philo	*philo;
-	t_data	*data;
+    t_philo	*philo;
+    t_data	*data;
 
-	philo = (t_philo *)arg;
-	data = philo->data;
-	while (!check_stop(data))
-	{
-		log_state(philo, "is thinking");
-		if (check_stop(data))
-			break ;
-		if (data->num_philos == 1)
-		{
-			log_state(philo, "has taken a fork");
-			precise_sleep(data->time_to_die * 1000);
-			log_state(philo, "died");
-			return (NULL);
-		}
-		check_left_right(philo);
-		check_routine(philo, data);
-		if (data->meals_required > 0
+    philo = (t_philo *)arg;
+    data = philo->data;
+    while (!check_stop(data))
+    {
+        log_state(philo, "is thinking");
+        if (check_stop(data))
+            break ;
+        if (data->num_philos == 1)
+        {
+            log_state(philo, "has taken a fork");
+            precise_sleep(data->time_to_die * 1000);
+            printf("died\n");
+            return (NULL);
+        }
+        check_left_right(philo);
+        check_routine(philo, data);
+        if (data->meals_required > 0
 			&& philo->meals_eaten >= data->meals_required)
 			break ;
 		log_state(philo, "is sleeping");
@@ -89,21 +89,21 @@ void	*philo_routine(void *arg)
 
 void	check_philo_death(t_data *data, int i, long now)
 {
-	long	time_since_last_meal;
-	int		meals_complete;
+    long	time_since_last_meal;
+    int		meals_complete;
 
-	pthread_mutex_lock(&data->philos[i].meal_mutex);
-	time_since_last_meal = now - data->philos[i].last_meal;
-	meals_complete = (data->meals_required > 0
-			&& data->philos[i].meals_eaten >= data->meals_required);
+    pthread_mutex_lock(&data->philos[i].meal_mutex);
+    time_since_last_meal = now - data->philos[i].last_meal;
+    meals_complete = (data->meals_required > 0
+            && data->philos[i].meals_eaten >= data->meals_required);
 	if (time_since_last_meal > data->time_to_die && !meals_complete)
 	{
 		pthread_mutex_unlock(&data->philos[i].meal_mutex);
 		set_stop(data);
-		log_state(&data->philos[i], "died");
+		printf("died\n");
 	}
-	else
-		pthread_mutex_unlock(&data->philos[i].meal_mutex);
+    else
+        pthread_mutex_unlock(&data->philos[i].meal_mutex);
 }
 
 void	*monitor_routine(void *arg)
